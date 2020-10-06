cda809fc0064872685ad19bfdc8af84e0dfd8704
Michael Blanchard <mblanchard@spock.highpoint.edu>
2020-10-06 12:34:59 -0400

    Lab 5 complete

--- lab5.py
+++ lab5.py
@@ -7,26 +7,30 @@ __license__ = "GPLv3"
 __version__ = "0.0.0"
 __maintainer__ = ""
 __email__ = "mblancha@highpoint.edu"
-__status__ = "Complete"
+__status__ = "Lab 5 Complete"
 
 def main():
    num = int(input("Enter a positive integer: "))
-   if num < 0:
-      print("The input must be a positive integer!")
-   elif num == float:
-      print("The input must be an integer!")
 
-   for i in range(2,num):
-      answer = i % num
-      if answer == 0:
-         composite = True
-      elif answer > 0:
-         composite = False
-   
-   if composite == True:
-      print("The number entered is composite.")
-   elif composite == False:
-      print("The number entered is prime.")
+   if num <= 0:
+      print("The input must be a positive integer!")
+   elif num == 1:
+      print(num, "is neither prime or composite!")
+   elif num == 2:
+      print(num, "is prime.")
+   else:
+      for i in range(2,num):
+         answer = num % i
+         if answer == 0:
+            composite = True
+            break
+         elif answer > 0:
+            composite = False     
+      if composite == False:
+         print("The number entered is prime.")
+      elif composite == True:
+         print("The number entered is composite.")
+      
 
 if __name__ == "__main__":
    main() 
